955bd6510e61aa191b5f9e3c3251f229b1301d0c
mtj-mjc <matej.mrnjec@exanic.ch>
2025-10-03 15:58:36 +0200

    feat: setup storybook with vite, react and tailwindcss

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,13 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest"
   ],
