a1d1c46084f17f542a15e195d5cd08c513ab39d8
Carla Iten <carla.iten@apptiva.ch>
2025-11-05 12:24:03 +0100

    clean up eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,40 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import storybook from "eslint-plugin-storybook";
 import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default defineConfig([
-  globalIgnores(["dist", "node_modules", "build", "storybook-static"]),
+export default [
+  // Global ignores
   {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ["dist", "node_modules", "build", "storybook-static"],
+  },
+  // Base JavaScript and TypeScript files
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
   },
+  // Storybook-specific files
+  ...storybook.configs["flat/recommended"],
+  // Prettier must be last to override other configs
   eslintConfigPrettier,
-]);
+];
