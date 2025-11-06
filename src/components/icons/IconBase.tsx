034fa47a3d32c3f2d6011f0a496a792da67c4a3a
Carla Iten <carla.iten@apptiva.ch>
2025-11-06 18:06:46 +0100

    added AccessibleIcon to IconBase

--- src/components/icons/IconBase.tsx
+++ src/components/icons/IconBase.tsx
@@ -1,18 +1,24 @@
+// src/components/icons/IconBase.tsx
+import { AccessibleIcon } from "@radix-ui/react-accessible-icon";
 import * as React from "react";
 
-export const IconBase: React.FC<React.SVGProps<SVGSVGElement>> = ({
+export type IconBaseProps = React.SVGProps<SVGSVGElement> & {
+  label?: string;
+};
+
+export const IconBase: React.FC<IconBaseProps> = ({
+  label = "",
   className,
-  children,
   ...props
 }) => (
-  <svg
-    width="16"
-    height="16"
-    className={`text-slate-600 ${className || ""}`}
-    fill="currentColor"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    {children}
-  </svg>
+  <AccessibleIcon label={label}>
+    <svg
+      width="16"
+      height="16"
+      className={`text-slate-600 ${className || ""}`}
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    />
+  </AccessibleIcon>
 );
