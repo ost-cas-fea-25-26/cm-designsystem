6c8e408e12799b26a72eea90bad15cc5fb7fe56f
Carla Iten <carla.iten@apptiva.ch>
2025-11-08 20:13:29 +0100

    cleanup

--- src/components/toggle/Toggle.tsx
+++ src/components/toggle/Toggle.tsx
@@ -4,14 +4,16 @@ import { HeartFilled, HeartOutline } from "../icons/generated";
 import { Label } from "../typography/Label";
 import type { ReactNode } from "react";
 
+
 const toggleStyles = tv({
   slots: {
-    root: [
+    
+    base: [
       // layout + spacing + shape
       "inline-flex items-center justify-center gap-2 h-8 px-3 py-2 rounded-full",
       // interaction & motion
       "transition-all duration-150 ease-in-out",
-      // focus styles & disabled state
+      // focus styles
       "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
       "disabled:opacity-50 disabled:pointer-events-none",
     ],
@@ -21,12 +23,12 @@ const toggleStyles = tv({
   variants: {
     pressed: {
       false: {
-        root: "bg-transparent hover:bg-pink-50 hover:text-pink-600",
+        base: "bg-transparent hover:bg-pink-50 hover:text-pink-600",
         icon: "text-inherit",
         label: "text-inherit",
       },
       true: {
-        root: "text-pink-900",
+        base: "text-pink-900",
         icon: "text-pink-500",
         label: "text-pink-900",
       },
@@ -37,6 +39,26 @@ const toggleStyles = tv({
   },
 });
 
+
+
+/*
+ * Varianten:
+ * Ohne Data
+ * - pressed: active / "Liked"
+ *  - todo: transition for 2s to "1 Like" after click
+ * - not pressed: 
+ *  - default: schwarzes "Like"
+ *  - hover: pinkes "Like" mit pinkem Hintergrund
+ * 
+ * Mit Data (Es gibt schon Likes)
+ * - pressed: active / "x Likes" (text: pink-900, icon: pink-500) + 1 like (achtung: state / data)
+ * - not pressed:
+ *  - default: "x Likes" (text: pink-900, icon: pink-500)
+ *  - hover: pinkes "x Likes" mit pinkem Hintergrund (text: pink-500, icon: pink-500)
+ */
+
+
+
 type ToggleVariants = VariantProps<typeof toggleStyles>;
 
 interface ToggleProps extends ToggleVariants {
@@ -46,10 +68,10 @@ interface ToggleProps extends ToggleVariants {
 }
 
 export const Toggle = ({ ariaLabel, pressed, children }: ToggleProps) => {
-  const { root, icon } = toggleStyles({ pressed });
+  const { base, icon } = toggleStyles({ pressed });
 
   return (
-    <RadixToggle.Root aria-label={ariaLabel} className={root()}>
+    <RadixToggle.Root aria-label={ariaLabel} className={base()}>
       <span className={icon()}>
         {pressed ? <HeartFilled /> : <HeartOutline />}
       </span>
