dc997e52588b432bdff7a5219f4df8c91e93af8f
Carla Iten <carla.iten@apptiva.ch>
2025-11-09 21:24:15 +0100

    changed app icon and favicon

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,14 @@
 import { addons } from "storybook/internal/manager-api";
 import { create } from "storybook/internal/theming";
 
+// Set favicon
+const link = (document.querySelector("link[rel*='icon']") ||
+  document.createElement("link")) as HTMLLinkElement;
+link.type = "image/svg+xml";
+link.rel = "icon";
+link.href = "/favicon.svg";
+document.getElementsByTagName("head")[0].appendChild(link);
+
 addons.setConfig({
   theme: create({
     base: "light",
