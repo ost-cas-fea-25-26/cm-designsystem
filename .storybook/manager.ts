1adf0800d87393561ea98381bbdcb69ecf49d408
Carla Iten <carla.iten@apptiva.ch>
2025-11-09 22:52:30 +0100

    fix icon path in storybook deployment

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -6,7 +6,7 @@ const link = (document.querySelector("link[rel*='icon']") ||
   document.createElement("link")) as HTMLLinkElement;
 link.type = "image/svg+xml";
 link.rel = "icon";
-link.href = "/favicon.svg";
+link.href = "./favicon.svg";
 document.getElementsByTagName("head")[0].appendChild(link);
 
 addons.setConfig({
@@ -14,7 +14,7 @@ addons.setConfig({
     base: "light",
     brandTitle: "Mumble Design System",
     brandUrl: "https://github.com/ost-cas-fea-25-26/cm-designsystem",
-    brandImage: "/logo-inline-gradient.svg",
+    brandImage: "./logo-inline-gradient.svg",
     brandTarget: "_self",
   }),
 });
