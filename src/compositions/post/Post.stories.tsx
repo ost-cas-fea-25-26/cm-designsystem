a95c4cfb53531d4c3a16d0cce2b26f370e4a53b3
mtj-mjc <matej.mrnjec@exanic.ch>
2025-12-11 22:03:47 +0100

    feat: display likes and comments in posts

--- src/compositions/post/Post.stories.tsx
+++ src/compositions/post/Post.stories.tsx
@@ -21,7 +21,11 @@ export const Large: Story = {
     userName: "UserName",
     displayName: "Display Name",
     timestamp: new Date(),
-    text: "Quia aut et aut. Sunt et eligendi similique enim qui quo minus. Aut aut error velit voluptatum optio sed quis cumque error magni. Deserunt pariatur molestiae incidunt. Omnis deserunt ratione et recusandae quos excepturi ut deleniti ut repellat magni.",
+    text: `Quia aut et aut. Sunt et eligendi similique enim qui quo minus. Aut aut error velit voluptatum optio sed quis cumque error magni. Deserunt pariatur molestiae incidunt. Omnis deserunt ratione et recusandae quos excepturi ut deleniti ut repellat magni.
+    
+#casfee #goOST #smartive #teamCM`,
+    nbrOfComments: 0,
+    nbrOfLikes: 0,
     onAvatarClick: fn(),
     onCommentClick: fn(),
     onLikeClick: fn(),
@@ -37,6 +41,8 @@ export const Medium: Story = {
     displayName: "Display Name",
     timestamp: new Date(),
     text: "Quia aut et aut. Sunt et eligendi similique enim qui quo minus. Aut aut error velit voluptatum optio sed quis cumque error magni. Deserunt pariatur molestiae incidunt. Omnis deserunt ratione et recusandae quos excepturi ut deleniti ut repellat magni.",
+    nbrOfComments: 0,
+    nbrOfLikes: 0,
     onAvatarClick: fn(),
     onCommentClick: fn(),
     onLikeClick: fn(),
@@ -55,6 +61,25 @@ export const PostWithImage: Story = {
     imageSrc:
       "https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80",
     imageAlt: "Profile banner image",
+    nbrOfComments: 0,
+    nbrOfLikes: 0,
+    onAvatarClick: fn(),
+    onCommentClick: fn(),
+    onLikeClick: fn(),
+    onShareClick: fn(),
+  },
+};
+
+export const LikesAndComments: Story = {
+  args: {
+    size: "md",
+    src: "",
+    userName: "UserName",
+    displayName: "Display Name",
+    timestamp: new Date(),
+    text: "Quia aut et aut. Sunt et eligendi similique enim qui quo minus. Aut aut error velit voluptatum optio sed quis cumque error magni. Deserunt pariatur molestiae incidunt. Omnis deserunt ratione et recusandae quos excepturi ut deleniti ut repellat magni.",
+    nbrOfComments: 10,
+    nbrOfLikes: 100,
     onAvatarClick: fn(),
     onCommentClick: fn(),
     onLikeClick: fn(),
--- src/compositions/response/Response.stories.tsx
+++ src/compositions/response/Response.stories.tsx
@@ -21,6 +21,8 @@ export const Default: Story = {
     displayName: "Display Name",
     timestamp: new Date(),
     text: "Quia aut et aut. Sunt et eligendi similique enim qui quo minus. Aut aut error velit voluptatum optio sed quis cumque error magni. Deserunt pariatur molestiae incidunt. Omnis deserunt ratione et recusandae quos excepturi ut deleniti ut repellat magni.",
+    nbrOfComments: 0,
+    nbrOfLikes: 0,
     onAvatarClick: fn(),
     onCommentClick: fn(),
     onLikeClick: fn(),
@@ -38,6 +40,26 @@ export const ResponseWithImage: Story = {
     imageSrc:
       "https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80",
     imageAlt: "Profile banner image",
+    nbrOfComments: 0,
+    nbrOfLikes: 0,
+    onAvatarClick: fn(),
+    onCommentClick: fn(),
+    onLikeClick: fn(),
+    onShareClick: fn(),
+  },
+};
+
+export const LikesAndComments: Story = {
+  args: {
+    src: "example",
+    userName: "UserName",
+    displayName: "Display Name",
+    timestamp: new Date(),
+    text: "Quia aut et aut. Sunt et eligendi similique enim qui quo minus. Aut aut error velit voluptatum optio sed quis cumque error magni. Deserunt pariatur molestiae incidunt. Omnis deserunt ratione et recusandae quos excepturi ut deleniti ut repellat magni.",
+    imageSrc: "",
+    imageAlt: "Profile banner image",
+    nbrOfComments: 10,
+    nbrOfLikes: 100,
     onAvatarClick: fn(),
     onCommentClick: fn(),
     onLikeClick: fn(),
