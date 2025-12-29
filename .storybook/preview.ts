7488a59f1e6807bd05496b57893a835326ea5003
Carla Iten <carlaiten@gmail.com>
2025-12-30 00:50:44 +0100

    fix: go back to exported .css (#342)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from "@storybook/react-vite";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 import "../src/index.css";
 
 const preview: Preview = {
